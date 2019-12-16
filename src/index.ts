e562b96bad7a790bb3df7a1a862c156297bfb551
Dejan Sandic <dejan.s@aleacontrol.com>
2019-12-16 17:57:17 +0100

    Add examples to the README.md

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import { createElement, createContext, useContext, useState, useEffect, ReactNode } from 'react';
 
+// Context
 const EmitterContext = createContext({});
 
+// ContextProvider
 function EmitterProvider ({ children = [], emitter }: { children: ReactNode; emitter: any }) {
    return createElement(EmitterContext.Provider, { value: emitter }, ...children);
 }
 
+// Use data for the specific event emitted by the emitter
 function useEmitter () {
    return useContext(EmitterContext);
 }
 
+// Use a full instance of the emitter with all of its methods.
 function useEvent (event: string, defaultValue?: any) {
    const emitter: any = useContext(EmitterContext);
    const [data, setData] = useState(defaultValue);
@@ -21,6 +25,7 @@ function useEvent (event: string, defaultValue?: any) {
    return data;
 }
 
+// Use data for the specific event emitted by the emitter only on the first dispatch
 useEvent.once = function (event: string, defaultValue?: any) {
    const emitter: any = useContext(EmitterContext);
    const [data, setData] = useState(defaultValue);
